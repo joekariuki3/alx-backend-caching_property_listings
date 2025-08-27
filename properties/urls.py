af1fa6edb5b2f87c43b1eba30dec99d09bde304d
joekariuki3 <joekariuki3@gmail.com>
2025-08-27 15:53:47 +0300

    Add URL routing for cache metrics view in "properties" app

--- properties/urls.py
+++ properties/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import property_list
+from .views import property_list, cache_metrics
 
 urlpatterns = [
     path('', property_list, name='property_list'),
+    path('cache_metrics/', cache_metrics, name='cache_metrics')
 ]
