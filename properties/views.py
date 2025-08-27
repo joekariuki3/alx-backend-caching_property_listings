85eb5ce7a6ee872fe0c3a527a772c2fff802662e
joekariuki3 <joekariuki3@gmail.com>
2025-08-27 14:53:33 +0300

    Add cached property list view with JSON response

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+
+@cache_page(60 * 15, key_prefix='property_list')
+def property_list(request):
+    if request.method == 'GET':
+        properties = Property.objects.all()
+        data = {'properties': list(properties.values())}
+        return JsonResponse(data=data, status=200, safe=False)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
