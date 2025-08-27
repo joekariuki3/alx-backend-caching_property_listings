45d31862b3dcdb9616346db1091cb2dad9afb2c0
joekariuki3 <joekariuki3@gmail.com>
2025-08-27 15:06:23 +0300

    Refactor property_list view to use get_all_properties utility function

--- properties/views.py
+++ properties/views.py
@@ -1,12 +1,13 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from .utils import get_all_properties
 
 
 @cache_page(60 * 15)
 def property_list(request):
     if request.method == 'GET':
-        properties = Property.objects.all()
+        properties = get_all_properties()
         data = {'properties': list(properties.values())}
         return JsonResponse(data=data, status=200, safe=False)
     else:
