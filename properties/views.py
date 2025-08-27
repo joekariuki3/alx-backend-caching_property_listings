5c83d78bafeafb63ad98ac787dbd32dcd8b4ce34
joekariuki3 <joekariuki3@gmail.com>
2025-08-27 14:55:19 +0300

    Remove unused key_prefix from property_list view cache decorator

--- properties/views.py
+++ properties/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.cache import cache_page
 from .models import Property
 
 
-@cache_page(60 * 15, key_prefix='property_list')
+@cache_page(60 * 15)
 def property_list(request):
     if request.method == 'GET':
         properties = Property.objects.all()
