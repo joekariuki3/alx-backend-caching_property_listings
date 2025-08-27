e74636294a7d7806ae7645d07c68663eae5e8e53
joekariuki3 <joekariuki3@gmail.com>
2025-08-27 15:54:20 +0300

    Add cache metrics view in "properties" app for retrieving Redis stats

--- properties/views.py
+++ properties/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
-from .utils import get_all_properties
+from .utils import get_all_properties, get_redis_cache_metrics
 
 
 @cache_page(60 * 15)
@@ -12,3 +12,8 @@ def property_list(request):
         return JsonResponse(data=data, status=200, safe=False)
     else:
         return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+def cache_metrics(request):
+    if request.method == 'GET':
+        metrics = get_redis_cache_metrics()
+        return JsonResponse(data=metrics, status=200)
